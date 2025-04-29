33b45c12cc6799e259af28785f208a6f13e8e6f9
Kim <kim.slabinskyi@gmail.com>
2025-04-29 14:04:31 +0200

    Initial Commit

---
+++ Cache App/Cache_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cache_AppApp.swift
+//  Cache App
+//
+//  Created by Kim on 29.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct Cache_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
